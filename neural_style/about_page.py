460c73bd5215b22eb39db4d0953b3e303e1bdbb4
FouziaFaria <88418502+FouziaFaria@users.noreply.github.com>
2021-08-10 16:20:04 +0600

    Add files via upload

---
+++ neural_style/about_page.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+
+def show_devs():
+    st.title('Hi!!!')
+
+    st.image("data//t.png", width = 200)
+    st.write("## I am **Tura**")
+    st.header("I'm a **2nd Year, 3rd Semester** student @ **[Daffodil International University](https://daffodilvarsity.edu.bd/)** ")
+    st.write("### This is my first Machine Learning Project named as **[Style Transfer](https://github.com/PrincessPride/ML-Salary-Prediction)**. \
+    **Style Transfer** is a Web App built in Streamlit.")
+    st.write("### I have a passion for art & crafts. I try to build things as visually appealing as possible. I have won many awards and prizes in my life as an Artist.")
+    
+    st.write("## Some of My **Works**:")
+    st.image("data//1.jpeg")
+    st.image("data//2.jpeg")
+    st.image("data//3.jpeg")
+    st.image("data//4.jpeg")
+    st.image("data//5.jpeg")
+    st.write("## My gift to our Previous Departmemt Head [Syed Akhter Hossain](https://en.wikipedia.org/wiki/Syed_Akhter_Hossain):")
+    st.image("data//6.jpeg")
+    st.image("data//sss.jpg")
+    st.write("## My Certificates:")
+    st.image("data//1.jpg")
---
+++ neural_style/upload.py
@@ -0,0 +1,56 @@
+# python3 -m venv venv
+# . venv/bin/activate
+# pip install streamlit
+# pip install torch torchvision
+# streamlit run main.py
+import streamlit as st
+from PIL import Image
+from streamlit import caching
+
+import style
+
+
+def upload_image():
+    global image
+
+    st.title('Style Transfer')
+
+    image_file = st.file_uploader("Upload", type=["png", "jpg"])
+
+    img = st.selectbox(
+        'Select Image',
+        ('amber.jpg', 'cat.png')
+    )
+
+    style_name = st.selectbox(
+        'Select Style',
+        ('candy', 'mosaic', 'rain_princess', 'udnie')
+    )
+
+
+    model= "saved_models/" + style_name + ".pth"
+    input_image = "images/content-images/" + img 
+    output_image = "images/output-images/" + style_name + "-" + img
+
+    if image_file is not None:
+        print("Successfully Uploaded!!!")
+        image = Image.open(image_file)
+    else:
+        image = Image.open(input_image)
+
+    st.write('### Source image:')
+    st.image(image, width=700) # image: numpy array
+
+    clicked = st.button('Stylize')
+
+    if clicked:
+        caching.clear_cache()
+        model = style.load_model(model)
+        if image_file is not None:
+            style.stylize(model, image_file, output_image)
+        else:
+            style.stylize(model, input_image, output_image)
+
+        st.write('### Output image:')
+        image = Image.open(output_image)
+        st.image(image, width=700)
